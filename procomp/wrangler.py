4747a52f7b1286e42ef318b3b1eb7ed2afaf4774
HarmonicHemispheres <robby.boney@wsu.edu>
2017-06-20 12:25:28 -0700

    added module for database query related functions

---
+++ procomp/wrangler.py
@@ -0,0 +1,19 @@
+
+"""
+OVERVIEW
+    functions in this module are focussed on query of ensembl
+    using the pycogent module to access the ensembl mysql database.
+"""
+
+def tr_to_pr(sp, tr):
+    """
+    OVERVIEW
+        searches the database for a species (sp) and aquires
+        the protein sequences associated with the provided
+        transcript id (tr).
+    INPUTS
+        sp = species to search
+        tr = transcript to convert to protein
+    """
+
+    return 0
