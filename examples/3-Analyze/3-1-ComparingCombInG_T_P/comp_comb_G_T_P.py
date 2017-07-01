1a24b14d51d224ddd3601cc1873fd7725d3eea03
HarmonicHemispheres <robby.boney@wsu.edu>
2017-06-30 18:29:54 -0700

    added summary of experiment

--- examples/3-Analyze/3-1-ComparingCombInG_T_P/comp_comb_G_T_P.py
+++ examples/3-Analyze/3-1-ComparingCombInG_T_P/comp_comb_G_T_P.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import
 import os
 import procomp.procomp as pc
+import statistics as sts
 
 """
 Here we generate combinations of transcripts that correlate to
@@ -32,57 +33,66 @@ def main():
     #
     # Get transcript results
     #
-    
     print("======== TRANSCRIPT ========")
     L = []
     for F in os.listdir(trans_dir):
         if ("." not in F):
             ali_dir = trans_dir + F + "/"
-            L += pc.comb_TrPr(ali_dir, spid, name_filter=" 1-500") 
+            L += pc.comb_IdPr(ali_dir, spid, name_filter=" 1-500") 
 
     L = sorted(L)
-    for i in L:
-        print(i)
     L = pc.comb_rm_dups(L, spid, ident="DART")
-    pc.comb_gen_combs(L, out_file, 500, ident="DART", refac=0)
+    L_Tr = pc.comb_gen_combs(L, out_file, 500, ident="DART", refac=0, w=0)
     
     
     #
     # Get Protein results
     #    
-    """
     print("======== PROTEIN ========")
     L = []
     for F in os.listdir(prot_dir):
         if ("." not in F):
             ali_dir = prot_dir + F + "/"
-            L += pc.comb_TrPr(ali_dir, spid) 
-            #print (L)
+            L += pc.comb_IdPr(ali_dir, spid) 
  
     L = sorted(L)
-    for i in L:
-        print(i)
     L = pc.comb_rm_dups(L, spid, ident="DARP") 
-    pc.comb_gen_combs(L, out_file, 500, ident="DARP", refac=0)
-    """
+    L_Pr = pc.comb_gen_combs(L, out_file, 500, ident="DARP", refac=0, w=0)
+    
     
     #
     # Get Gene results
     #    
-    """
     print("======== GENE ========")
     L = []
     for F in os.listdir(gene_dir):
         if ("." not in F):
             ali_dir = gene_dir + F + "/"
-            L += pc.comb_TrPr(ali_dir, spid) 
+            L += pc.comb_IdPr(ali_dir, spid) 
  
     L = sorted(L)
-    
     L = pc.comb_rm_dups(L, spid, ident="DARG")
+    L_Gn = pc.comb_gen_combs(L, out_file, 500, ident="DARG", refac=0, w=0)
+
     
-    pc.comb_gen_combs(L, out_file, 500, ident="DARG", refac=0)
-    """
+    #
+    # Visualize Data
+    #
+    print("\n\n======== RESULTS ========")
+    L_Gn_comb = [int(i.split()[-1]) for i in L_Gn]
+    L_Tr_comb = [int(i.split()[-1]) for i in L_Tr]
+    L_Pr_comb = [int(i.split()[-1]) for i in L_Pr]
+    print("Gene        Average Combinations:", sum(L_Gn_comb)/len(L_Gn_comb) )
+    print("Transcript  Average Combinations:", sum(L_Tr_comb)/len(L_Tr_comb) )
+    print("Protein     Average Combinations:", sum(L_Pr_comb)/len(L_Pr_comb) )
+    
+    print("\nGene        Median Combinations:", sts.median(L_Gn_comb) )
+    print("Transcript  Median Combinations:", sts.median(L_Tr_comb) )
+    print("Protein     Median Combinations:", sts.median(L_Pr_comb) )
+
+    print("======== ======== ========\n\n")
+
+    
 if __name__ == '__main__':
     main()
 
---
+++ examples/3-Analyze/3-1-ComparingCombInG_T_P/overview.md
@@ -0,0 +1,14 @@
+## Comparing Gene, Transcript and Protein for Combination Count
+
+this experiment aims to determine which method of orthologs provide the fewest number of combinations of alignments needed in the next step of the analysis.
+
+## Methods
+
+As demonstrated in the script to do this comparison we compute the number of combinations for each id given (either gene, transcript, protein) then get average and median combinations for each category and display them in results.
+
+## Results
+
+|Statistic |Gene |Transcript |Protein |
+|----------|-----|-----------|--------|
+|Average Combination |2.827e+60 |1.118e+60 |1.120e+60 |
+|Median Combinations |8 |4 | 4|
