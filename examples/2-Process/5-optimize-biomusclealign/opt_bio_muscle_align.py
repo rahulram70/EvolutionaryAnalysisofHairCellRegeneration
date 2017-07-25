2df78ea83841daa9f0f0ce9dc85c28ec1a78016b
HarmonicHemispheres <robby.boney@wsu.edu>
2017-07-25 16:27:19 -0700

    added multithreading(MT) R&D experiment to MT biomusclealign()

---
+++ examples/2-Process/5-optimize-biomusclealign/opt_bio_muscle_align.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+def main():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    res_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+
+    # go up 2 directories to project dir
+    for i in range(2):
+        res_dir = os.path.abspath(os.path.join(res_dir, os.pardir))
+
+    # add project to path and import package
+    sys.path.append(res_dir)
+    import procomp as pc
+
+    # Setup paths to needed directories
+    #
+    in_fl = res_dir + "/resources/data-cleaned/aligned_seq/ENSDART00000000042_0 .txt"
+
+    print(pc.__doc__)
+    # original function
+    #pc.bioMuscleAlign()
+
+    # 
+    
+if __name__ == '__main__':
+    main()
---
+++ examples/2-Process/5-optimize-biomusclealign/overvew.md
@@ -0,0 +1,8 @@
+# Making BioMuscleAlgin() a multithreaded function
+This project aims to multithread this function due to it being a bottleneck for our computation. being able to run an alignment in a short amount of time will allow for quick turnaround in analysis which can be beneficial to our proejct. 
+
+Multithreading is the concep...
+
+## Methods
+
+## Results
