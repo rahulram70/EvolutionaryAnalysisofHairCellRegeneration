61d2c3ae634a47a50ac8454ae749726965da5190
rahulram70 <rahulram70@gmail.com>
2017-07-27 17:49:09 -0700

    Added threading

--- examples/2-Process/5-optimize-biomusclealign/opt_bio_muscle_align.py
+++ examples/2-Process/5-optimize-biomusclealign/opt_bio_muscle_align.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import time
 
 def main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -19,20 +20,41 @@ def main():
     #
     
     out_dir = script_dir + "/post-align/"
+    in_dir1 = script_dir + "/pre_align1/"
+    in_dir2 = script_dir + "/pre_align2/"
     in_dir = script_dir + "/pre-align/"
-    musl_path = res_dir + "/resources/muscle/muscle3.8.31_i86darwin64.31_i86darwin64"
-    
+    musl_path = res_dir + "/resources/muscle/muscle3.8.31_i86win32.exe"
+    inputL = []
     # ---------------------------------
     # original muscle align function
     #
-    pc.bioMuscleAlign(in_dir, musl_path, outputF=out_dir)
-
-    # ------------------------------
+    # start = time.time()
+    # pc.bioMuscleAlign(in_dir, musl_path, outputF=out_dir)
+    # end = time.time()
+    # print(end - start)
+    # # # ------------------------------
     # New multithreading based alignments
     #
+    # start = time.time()
+    # thread1 = pc.myThread(in_dir1, musl_path, outputF=out_dir)    
+    # thread2 = pc.myThread(in_dir2, musl_path, outputF=out_dir)
     
+    
+    # thread1.start()
+    # thread2.start()
+    # end = time.time()
+    # print(end - start)
+    # for file_name in os.listdir(in_dir1):
+    #     full_file_name = in_dir1 + file_name
+    #     inputL.append(full_file_name)
+    
+    #print(inputL)
+    #pc.bioMuscleAlignList(inputL, musl_path, out_dir)
 
+    threadL = pc.thread_creator(in_dir, 10, out_dir, musl_path)
 
+    for obj in threadL:
+        obj.start()
     
 if __name__ == '__main__':
     main()
