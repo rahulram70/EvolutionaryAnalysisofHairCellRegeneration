688dfe1134a702f67c5c8e7aa23d19f9297fe028
rahulram70 <rahulram70@gmail.com>
2017-08-09 14:32:39 -0700

    Download Gene and protein scripts

--- examples/1-Wrangle/5-ncbi_gath_protiens/gath_proteins.py
+++ examples/1-Wrangle/5-ncbi_gath_protiens/gath_proteins.py
@@ -4,8 +4,6 @@ import os
 import sys
 from Bio import Entrez
 from Bio import SeqIO
-from Bio.Blast import NCBIWWW
-from Bio.Blast import NCBIXML
 
 def main():
     """
@@ -24,8 +22,9 @@ def main():
     #
     pl_fl = script_dir + "/gather_domain_info.pl"
     genes_path = res_dir + "/resources/data-raw/gene_names.txt"
-    gene_out = res_dir + "/resources/data-raw/gene_list.txt"
+    gene_output_path = res_dir + "/resources/data-raw/Zebrafish_Protein_Sequences.txt"
     res_dir += "/resources/data-raw/protein-sequences/"
+     
 
 
     #
@@ -33,50 +32,102 @@ def main():
     #
     genes = open(genes_path, "r")
     genes_L = genes.read().splitlines()
-
+    gene_output = open(gene_output_path, "w+")
+    
+    genes_L.remove(genes_L[0])
     #
     # Set up necessary variables
-    #
-    organism = "Danio rerio"
-    stable_id = genes_L[3].split(", ")[1]
-    term = "{}[Gene] AND {}[Organism]".format(stable_id, organism)
-    print(term)
+    # >{}, {}, {}, {}\n".format(seq_record.id, " ".join(accession_list), stable_id, seq_record.seq)
+    gene_output.write("Sequence Record ID, Gene, Protein Sequence\n")
+    for i in range(len(genes_L)):
+        
+        organism = "Danio rerio"
+        stable_id = genes_L[i].split(", ")[1].split()[0]
+        term = "{}[Gene] AND {}[Organism]".format(stable_id, organism)
+        #print(term)
+        print(stable_id)
+        #
+        # ncbi login and query
+        #
+        Entrez.email = "harmonichemispheres@gmail.com"     # Always tell NCBI who you are
 
-    #
-    # ncbi login and query
-    #
-    Entrez.email = "harmonichemispheres@gmail.com"     # Always tell NCBI who you are
+        # Search
+        handle = Entrez.esearch(db="Protein", term=term)   #, retmax='100'
 
-    # Search
-    handle = Entrez.esearch(db="Protein", term=stable_id)   #, retmax='100'
+        # Get search string to search for ids
+        record = Entrez.read(handle)
+        
+        ids = record['IdList']
+        #print("{} hits".format(len(ids)))
+        #for i in ids:
+        #    print(i)
 
-    # Get search string to search for ids
-    record = Entrez.read(handle)
-    ids = record['IdList']
-    print("{} hits".format(len(ids)))
-    #for i in ids:
-    #    print(i)
+        # choose id to search
+        
+        
+        #print(ids)
 
-    # choose id to search
-    seq_id = ids[0]
+        # setup entrez handle 
+        
+        
+        
+        handle2 = Entrez.efetch(db="Protein", id=ids, rettype="gb", retmode="text")
 
-    # setup entrez handle 
-    handle2 = Entrez.efetch(db="Protein", id=ids, rettype="gb", retmode="text")
+        
 
-    # ------------------------------
-    # Get zebrafish protein and id
-    #
-    seq = ""
-    for seq_record in SeqIO.parse(handle2, "gb"):
-        if (organism in seq_record.annotations["source"]):
-            print("{}_{}_len={}".format(seq_record.id, \
-                seq_record.annotations["source"], len(seq_record)) )
-            seq += "{}>{}".format(seq_record.annotations["source"], seq_record.seq)
-            print(seq) 
-            break
-    
-    # Close Entrez database handle
-    handle.close()
+            # ------------------------------
+            # Get zebrafish protein and id
+            #
+            
+        seq = ""
+        count = 0
+        
+        longest_id = ""
+        parse_handle = list(SeqIO.parse(handle2, "gb"))
+        count = 0
+        for seq_record in parse_handle:        
+            
+            if ( "NP_" in seq_record.id):
+                print(seq_record.id + " NP confirmed")
+                count += 1
+                
+                    #print(seq_record)
+                    #print(seq_record.annotations["source"])
+                    #print(stable_id)              
+                    #accession_list = seq_record.annotations["accessions"]
+                    #print(">{}, {}".format(seq_record.id, ", ".join(accession_list)))
+                    #print("len={}".format(len(seq_record.seq)))
+                gene_output.write("{},  {}, {}\n".format(seq_record.id, stable_id, seq_record.seq))
+                    #print(">{}, {}, {}\n".format(seq_record.id, stable_id, seq_record.seq))
+                
+
+        
+        if(count == 0):
+            longest_rec = 0
+            print("No NR found")       
+            for element in parse_handle:
+                print(len(element.seq))
+                print(longest_rec)
+                if("XP" in element.id and len(element.seq)> longest_rec):
+                    longest_id = element.id
+                    longest_rec = len(element.seq)
+            
+            gene_output.write("{},  {}, {}\n".format(element.id, stable_id, element.seq))
+            print(stable_id + ": " + longest_id)
+        
+            if(longest_rec == 0):
+                gene_output.write("{},  {}, {}\n".format("Error", stable_id, "Error"))
+                     
+            
+
+                
+
+
+                
+                
+        
+        # Close Entrez database handle
+        handle.close()
 
 if __name__ == '__main__':
     main()
